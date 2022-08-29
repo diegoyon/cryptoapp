dd59503fe152fa703dd20c7539afa5df07ddf428
Diego Yon <diegoyon@outlook.com>
2022-08-29 09:43:35 -0600

    Setup pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
+import Home from "./pages/Home";
+import Details from './pages/Details';
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/details" exact element={<Details />} />
+      </Routes>
+    </Router>
   );
 }
 
