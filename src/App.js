54520f5591fab94abf8d59b28f43e21a34c6f3be
Diego Yon <diegoyon@outlook.com>
2022-08-29 09:47:08 -0600

    Install linters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {
   BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
-import Home from "./pages/Home";
+import Home from './pages/Home';
 import Details from './pages/Details';
 import './App.css';
 
