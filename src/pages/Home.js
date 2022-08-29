b103c139ec84ac4e5afd030091814a9c42d3a1d1
Diego Yon <diegoyon@outlook.com>
2022-08-29 11:02:34 -0600

    fetch assets

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Home() {
+  const assets = useSelector((state) => state.assets.assets)
+  const status = useSelector((state) => state.assets.status)
+
+  let content;
+  if(status === 'loading'){
+    content = <p>Loading</p>
+  }
+  if (status === 'succeeded'){
+    content = assets.map((asset) => (
+      <div key={asset.id}>
+        <p>{asset.id}</p>
+        <p>{asset.rank}</p>
+      </div>
+    ))
+  }
+
   return (
-    <div>Home</div>
+    <div>{content}</div>
   );
 }
 
---
+++ src/redux/Assets.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const ASSETS_URL = 'https://api.coincap.io/v2/assets';
+
+export const fetchAssets = createAsyncThunk(
+  'assets/fetchAssets',
+  async () => {
+    const response = await axios.get(ASSETS_URL);
+    // console.log(response.data.data);
+    return response.data.data;
+  }
+)
+
+export const assets = createSlice({
+  name: 'assets',
+  initialState: {
+    assets: [],
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchAssets.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAssets.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.assets = action.payload;
+      })
+  }
+})
+
+export default assets.reducer;
